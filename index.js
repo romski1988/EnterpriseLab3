4b6974225d6f9a055bbfe7ea687bff563dad4c14
Roman <romski1988@gmail.com>
2021-02-04 16:51:53 +0000

    lab3 Index.html

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var http = require("http");
+fs = require('fs');
 const  PORT = process.env.PORT || 3000;
 
 http.createServer(function (request, response){
     // Send the HTTP header
     // HTTP Status: 200 : OK
     // Content Type: text/plain
-    response.writeHead(200, {'Content-Type':'text/plain'});
 
+    // Write to our server. set configuration for the response
+    response.writeHead(200, {
+        'Content-Type' : 'text/html',
+        'Access-Control-Allow-Origin' : '*'
+    });
+    var readStream = fs.createReadStream(__dirname + '/index.html');
     // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    readStream.pipe(response);
 }).listen(PORT, () => {
     console.log('our app is running on port ${ PORT }');
 });

6ced94d170989cb979d18ca72ac75ae048d2a72d
Roman <romski1988@gmail.com>
2021-02-04 17:56:22 +0000

    lab3 Routes to pages

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
 
+// Create routes for admin section
+// Get an instance of the router
+var adminRouter = express.Router();
+
+// Admin main page. the dashboard (http://localhost:PORT/admin)
+adminRouter.get('/', function(req, res){
+    res.send('I am the Dashboard');
+});
+
+// User page (http://localhost:PORT/admin/users)
+adminRouter.get('/users', function(res, req){
+    res.send('I show you all users');
+});
+
+// Post page (http://localhost:PORT/admin/posts)
+adminRouter.get('/posts', function(res, req){
+    res.send('I show you posts')
+});
+
+// Apply the routes to our application
+app.use('/admin', adminRouter);
+
 // Start Server
 app.listen(PORT);
 console.log('Express Server running at http://127.0.0.1:'.PORT);
